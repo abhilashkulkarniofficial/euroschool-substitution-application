2238603e78551e17c042a7558e319b065523fc23
abhilashkulkarni <abhilashkulkarniofficial@gmail.com>
2020-09-03 22:26:54 +0530

    First commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,179 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container fluid>
+    <v-row align="center">
+      <v-col class="d-flex" cols="12" sm="6">
+        <v-select
+          v-model="classId"
+          :items="classes"
+          label="Classes"
+        ></v-select>
+      </v-col>
+
+      <v-col class="d-flex" cols="12" sm="6">
+        <v-btn text color="primary" outlined @click="getTimetable">Submit</v-btn>
+      </v-col>
+    </v-row>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
+
+<v-row align="center">
+      <v-col class="d-flex" cols="12" sm="2">
+        <v-select
+          v-model="selectedElement.day"
+          :items="days"
+          label="Days"
+        ></v-select>
+      </v-col>
+
+      <v-col class="d-flex" cols="12" sm="2">
+        <v-select
+          v-model="selectedElement.class"
+          :items="classes"
+          label="Classes"
+        ></v-select>
+      </v-col>
+
+      <v-col class="d-flex" cols="12" sm="2">
+        <v-select
+          v-model="selectedElement.subject"
+          :items="subjects"
+          label="Subjects"
+        ></v-select>
+      </v-col>
+
+      <v-col class="d-flex" cols="12" sm="2">
+        <v-btn text color="primary" outlined @click="getTeachers">Submit</v-btn>
+      </v-col>
+
+      <v-col class="d-flex" cols="12" sm="2">
+        <v-btn text color="primary" outlined @click="getTeachers">Clear</v-btn>
+      </v-col>
+    </v-row>
+
+<v-row align="center">
+<v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">Teacher Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in teacherList" :key="item">
+          <td>{{ item }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</v-row>
+
+
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import axios from 'axios';
+import json from '../assets/teachers.json'
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Day / Period',
+            align: 'start',
+            sortable: false,
+            value: 'day',
+          },
+          { text: '1st', value: 'first' },
+          { text: '2nd', value: 'second' },
+          { text: '3rd', value: 'third' },
+          { text: '4th', value: 'fourth' },
+          { text: '5th', value: 'fifth' },
+        ],
+        desserts: [],
+        classId: '',
+        classes:['1A','1B','1C','1D','1E'],
+        subjects:["2LK","CK","CH","CH/K","3LH/K","Eng"],
+        teachersJson:json,
+        teachers:{},
+        days:['Monday','Tuesday','Wednesday','Thursday','Friday'],
+        selectedElement:{
+          day:null,
+          class:null,
+          subject:null
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+        },
+        teacherList:[]
+      }
+    },
+    mounted(){
+      for(var k in this.teachersJson) this.teachers[k] = true
+      console.log(this.teachers)
+    },
+    methods:{
+      getTimetable: function(){
+        axios.get('http://127.0.0.1:3000/timetable',{
+          params: {
+            classId: this.classId
+          }
+        }).then((response) => {
+          // console.log(response.data)
+          this.desserts = response.data
+        })
+        // console.log(this.classId)
+      },
+      getTeachers(){
+        this.reset()
+        console.log(this.selectedElement)
+        if(this.selectedElement.class){
+          for(let teacher in this.teachersJson){
+            if(!this.teachersJson[teacher].classList.includes(this.selectedElement.class)){
+              this.teachers[teacher] = false
+            }
+          }
+        }
+        // if(this.selectedElement.subject){
+        //   for(var teacher in this.teachersJson){
+        //     if(!this.teachersJson[teacher].subjects.includes(this.selectedElement.subject)){
+        //       this.teachers[teacher] = false
+        //     }
+        //   }
+        // }
+
+        if(this.selectedElement.subject){
+          for(let teacher in this.teachersJson){
+            if(!this.teachersJson[teacher].subjects.includes(this.selectedElement.subject)){
+              this.teachers[teacher] = false
+            }
+          }
+        }
+        for(var k in this.teachers){
+          if(this.teachers[k]){
+            this.teacherList.push(k)
+          }
+        }
+        console.log(this.teacherList)
+      },
+      clear(){
+        this.selectedElement = {
+          day:null,
+          class:null,
+          subject:null
+
+        }
+        this.teacherList = []
+        for(var k in this.teachersJson) this.teachers[k] = true
+      },
+      reset(){
+        this.teacherList = []
+        for(var k in this.teachersJson) this.teachers[k] = true
+      }
+
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex, VueAxios, axios)
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
